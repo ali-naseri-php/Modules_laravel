cef44279b56a322dd341e38b6968b0d256791a11
ali <ali.naseri.php@gmail.com>
2024-09-28 19:57:52 +0330

    add cache all category and all propertei

--- Modules/Category/app/Services/Category/AllCategoryServics.php
+++ Modules/Category/app/Services/Category/AllCategoryServics.php
@@ -2,14 +2,26 @@
 
 namespace Modules\Category\Services\Category;
 
+use Illuminate\Support\Facades\Cache;
 use Modules\Category\Models\Category;
 
 
 class  AllCategoryServics
 {
-    public function all_category($page=1)
+    public function all_category($page = 1)
     {
-        return Category::orderBy('parent_category' ,'asc')->paginate(1);
+        if ($page == 1 or $page==null) {
+
+            $data = Cache::remember('category', 120, function () {
+//                sleep(5);
+                $data= Category::orderBy('parent_category', 'asc')->paginate(2);
+                return $data;
+            });
+
+        } else {
+            $data = Category::orderBy('parent_category', 'asc')->paginate(2);
+        }
+        return $data;
 
     }
 
--- Modules/Category/app/Services/Propertie/AllPropertieServies.php
+++ Modules/Category/app/Services/Propertie/AllPropertieServies.php
@@ -1,15 +1,31 @@
 <?php
+
 namespace Modules\Category\Services\Propertie;
 
+use Illuminate\Support\Facades\Cache;
+use Modules\Category\Models\Category;
 use Modules\Category\Models\Propertie;
 
 class AllPropertieServies
 {
     public function all($page)
     {
-//        dd($page);
-return Propertie::paginate(5);
- }
+        //        dd($page);
+
+        if ($page == 1 or $page==null) {
+
+            $data = Cache::remember('propertei', 120, function () {
+                //                sleep(5);
+                $data= Propertie::paginate(5);
+                return $data;
+            });
+
+        } else {
+            $data = Propertie::paginate(5);
+        }
+        return $data;
+
+    }
 }
 
 
