82ff5ffe28018204f5805a2448137134234d20b6
ali <ali.naseri.php@gmail.com>
2024-10-29 16:11:25 +0330

    add order by for kala price most

--- Modules/ProductDisplay/app/Livewire/Kala/IndexKala.php
+++ Modules/ProductDisplay/app/Livewire/Kala/IndexKala.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 use Modules\ProductDisplay\Services\Kala\AllCategoryServices;
 use Modules\ProductDisplay\Services\Kala\AllKalaOrderByNewDataServices;
 use Modules\ProductDisplay\Services\Kala\AllKalaOrderByPriceLeastServices;
+use Modules\ProductDisplay\Services\Kala\AllKalaOrderByPriceMostServices;
 use Modules\ProductDisplay\Services\Kala\AllKalaServices;
 
 class IndexKala extends Component
@@ -18,26 +19,29 @@ class IndexKala extends Component
         //'قیمت  کم ترین '
         if ($request->q == 2) {
             $this->order_by_price_least();
-        } //'قیمت بیشترین '
+        }
+        //'قیمت بیشترین '
         elseif ($request->q == 3) {
 
+            $this->order_by_price_most();
 
-            dd('قیمت بیشترین ');
         } //"بر اساس بازدی "
         elseif ($request->q == 4) {
             dd("بر اساس بازدی ");
         } else {
             $this->order_by_new();
         }
-
+        dd($this->kalas);
+    }
+    public function order_by_price_most()
+    {// بارگذاری تنبل سرویس فقط هنگام نیاز
+        $kala = resolve(AllKalaOrderByPriceMostServices::class);
+        $this->kalas = $kala->all();
     }
     public function order_by_price_least()
-    {
-        // بارگذاری تنبل سرویس فقط هنگام نیاز
+    {// بارگذاری تنبل سرویس فقط هنگام نیاز
         $kala = resolve(AllKalaOrderByPriceLeastServices::class);
         $this->kalas = $kala->all();
-
-
     }
 
     public function order_by_new()
---
+++ Modules/ProductDisplay/app/Services/Kala/AllKalaOrderByPriceMostServices.php
@@ -0,0 +1,35 @@
+<?php
+namespace Modules\ProductDisplay\Services\Kala;
+
+use Illuminate\Support\Facades\Cache;
+use Modules\ProductManagement\Models\Category;
+use \Modules\ProductDisplay\Models\Kala;
+
+
+class AllKalaOrderByPriceMostServices
+{
+    protected $page;
+    public function __construct($page)
+    {
+        $this->page=$page;
+    }
+
+
+    public function all()
+    {
+        if ($this->page == 1 ) {
+            $data = Cache::remember('kala_price_most', 120, function () {
+                //                sleep(5);
+                $data = Kala::orderBy('price','DESC')->paginate(5);
+                return $data;
+            });
+
+        } else {
+            $data = Kala::orderBy('price','DESC')->paginate(5);
+        }
+        return $data->all();
+
+    }
+
+
+}
