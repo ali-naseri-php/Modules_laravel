8792f17dbdf5a17c946c2e31bd4c74358693c4f0
ali <ali.naseri.php@gmail.com>
2024-11-15 22:14:33 +0330

    Fix:  bug add class

--- Modules/ProductDisplay/app/Livewire/KalaCategory/AllKalaNoPropertis.php
+++ Modules/ProductDisplay/app/Livewire/KalaCategory/AllKalaNoPropertis.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Livewire\Component;
 use Modules\ProductDisplay\Http\Requests\AllKalaNoPropertisRequest;
 use Modules\ProductDisplay\Services\KalaNoPropertis\AllKalaOrderByNewServices;
+use Modules\ProductDisplay\Services\KalaNoPropertis\AllKalaOrderByPriceLeastServices;
 use Modules\ProductDisplay\Services\KalaNoPropertis\AllKalaOrderByPriceMostServices;
 use Modules\ProductDisplay\Services\KalaNoPropertis\AllKalaOrderByVisitServices;
 
