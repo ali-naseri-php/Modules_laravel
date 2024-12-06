f2d24cb9703cd977a71373be119aba5235590d4d
ali <ali.naseri.php@gmail.com>
2024-12-06 21:45:06 +0330

    fix : bog in form role-user

---
+++ Modules/Account/app/Livewire/FormRoleUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Account\Livewire;
+
+use Livewire\Component;
+
+class FormRoleUser extends Component
+{
+    public function render()
+    {
+        return view('account::livewire.form-role-user');
+    }
+}
