f09ee058a3ac0bc51263f8c0b0291b1baea57bcb
ali <ali.naseri.php@gmail.com>
2024-12-24 16:29:42 +0330

    add css and js for app and component livewire

--- Modules/HomepageModule/resources/views/layouts/app.blade.php
+++ Modules/HomepageModule/resources/views/layouts/app.blade.php
@@ -6,8 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <!-- ... -->
+    @livewireStyles
+    @stack('styles')
 </head>
 <body>
-
+{{ $slot }}
+@livewireScripts
+@livewireScriptConfig
+@stack('scripts')
 </body>
 </html>
