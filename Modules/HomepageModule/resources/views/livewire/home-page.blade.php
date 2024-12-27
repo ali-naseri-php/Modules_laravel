a7bd5d0b71c635094c92ba41781251b1b2fddb30
ali <ali.naseri.php@gmail.com>
2024-12-27 10:49:30 +0330

    add search blade for homepage and if is blade file!

--- Modules/HomepageModule/resources/views/livewire/home-page.blade.php
+++ Modules/HomepageModule/resources/views/livewire/home-page.blade.php
@@ -1,6 +1,8 @@
 <div>
-    @if(request()->is('/login'))
-        {{'ali naseri'}}
+    @if (view()->exists('search::livewire.form-search'))
+        <livewire:search::form-search />
     @endif
+
+
 
 </div>
